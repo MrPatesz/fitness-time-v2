ee5b735e7738d49273832c37ccbb83b9eecc4afa
Patrik Ángyán <patesz1999@gmail.com>
2023-07-09 13:11:14 +0200

    removed unnecessary code

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -3,14 +3,6 @@ import {createNextApiHandler} from "@trpc/server/adapters/next";
 import {env} from "../../../env.js";
 import {createTRPCContext} from "../../../server/api/trpc";
 import {appRouter} from "../../../server/api/root";
-import {signOut} from "next-auth/react";
-import {TRPC_ERROR_CODE_KEY} from "@trpc/server/src/rpc/codes";
-
-const signOutOnUnauthorized = (statusCode: TRPC_ERROR_CODE_KEY) => {
-  if (statusCode === "UNAUTHORIZED") {
-    signOut({callbackUrl: "/welcome"});
-  }
-};
 
 // export API handler
 export default createNextApiHandler({
@@ -18,11 +10,7 @@ export default createNextApiHandler({
   createContext: createTRPCContext,
   onError:
     env.NODE_ENV === "development"
-      ? ({path, error}) => {
-        console.error(
-          `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-        );
-        signOutOnUnauthorized(error.code);
-      }
-      : ({error}) => signOutOnUnauthorized(error.code),
+      ? ({path, error}) => console.error(
+        `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+      ) : undefined,
 });
