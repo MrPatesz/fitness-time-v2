4c949ea4278951f408e59e4aa493687aa7099e75
Patrik Ángyán <patesz1999@gmail.com>
2023-07-23 12:06:31 +0200

    fixed useFormatter bug

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,47 +1,39 @@
 import {useRouter} from "next/router";
-import {useState} from "react";
+import {useMemo} from "react";
 
 export const usePriceFormatter = () => {
   const {locale = "en"} = useRouter();
-  const [priceFormatter] = useState(() => new Intl.NumberFormat(locale, {
+  return useMemo(() => new Intl.NumberFormat(locale, {
     style: "currency",
     currency: "USD",
     maximumFractionDigits: 0,
-  }));
-
-  return priceFormatter;
+  }), [locale]);
 };
 
 export const useLongDateFormatter = () => {
   const {locale = "en"} = useRouter();
-  const [longDateFormatter] = useState(() => new Intl.DateTimeFormat(locale, {
+  return useMemo(() => new Intl.DateTimeFormat(locale, {
     year: "numeric",
     month: "long",
     day: "2-digit",
     hour: "2-digit",
     minute: "2-digit",
     hour12: false,
-  }));
-
-  return longDateFormatter;
+  }), [locale]);
 };
 
 export const useShortDateFormatter = () => {
   const {locale = "en"} = useRouter();
-  const [shortDateFormatter] = useState(() => new Intl.DateTimeFormat(locale, {
+  return useMemo(() => new Intl.DateTimeFormat(locale, {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
-  }));
-
-  return shortDateFormatter;
+  }), [locale]);
 };
 
 export const useSignedNumberFormatter = () => {
   const {locale = "en"} = useRouter();
-  const [signedNumberFormatter] = useState(() => new Intl.NumberFormat(locale, {
+  return useMemo(() => new Intl.NumberFormat(locale, {
     signDisplay: "exceptZero",
-  }));
-
-  return signedNumberFormatter;
+  }), [locale]);
 };
