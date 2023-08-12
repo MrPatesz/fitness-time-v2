4f2bd8e0771a81e2b34b43bd72cedbec65a7feb4
Patrik Ángyán <patesz1999@gmail.com>
2023-08-12 14:34:38 +0200

    PropsWithChildren in QueryComponent

--- src/components/QueryComponent.tsx
+++ src/components/QueryComponent.tsx
@@ -1,21 +1,20 @@
 import {Card, Progress} from '@mantine/core';
 import {UseTRPCQueryResult} from '@trpc/react-query/shared';
 import {useTranslation} from 'next-i18next';
-import {FunctionComponent, useEffect} from 'react';
+import {FunctionComponent, PropsWithChildren, useEffect} from 'react';
 import {CenteredLoader} from './CenteredLoader';
 import {EventInfo, usePusher} from '../hooks/usePusher';
 import {OverlayLoader} from './OverlayLoader';
 
 // TODO generic component: query.data has to match setState arg
 // TODO consider passing in a function to render children -> .data won't be undefined there
-export const QueryComponent: FunctionComponent<{
-  resourceName: string;
+export const QueryComponent: FunctionComponent<PropsWithChildren<{
   query: UseTRPCQueryResult<unknown, unknown>;
-  children: JSX.Element | JSX.Element[] | string | undefined | null;
+  resourceName: string;
   setState?: (newState: unknown) => void;
   eventInfo?: EventInfo;
-  loading?: boolean; // TODO pass this in from mutations
-}> = ({resourceName, query, children, setState, eventInfo, loading}) => {
+  loading?: boolean;
+}>> = ({resourceName, query, children, setState, eventInfo, loading}) => {
   const {t} = useTranslation('common');
 
   useEffect(() => {
