33b80839aed5aefd57fde10f42f65455b5a3ece1
Patrik Ángyán <patesz1999@gmail.com>
2023-10-04 16:12:43 +0200

    mantineFormUtils type-safety

--- src/utils/mantineFormUtils.ts
+++ src/utils/mantineFormUtils.ts
@@ -1,20 +1,17 @@
 import {UseFormReturnType} from '@mantine/form';
 import {CreateLocationType} from '../models/Location';
 
-// TODO type-safety
+export const getFormStringValue = <T, Key extends keyof T>(form: UseFormReturnType<T>, path: Key) =>
+  (form.getInputProps(path).value as string | undefined) ?? '';
 
-export const getFormStringValue = (form: UseFormReturnType<any>, path: string) => (form.getInputProps(path).value as string | undefined) ?? '';
+export const getFormStringOnChange = <T, Key extends keyof T>(form: UseFormReturnType<T>, path: Key) =>
+  (form.getInputProps(path).onChange as (newValue: string) => void);
 
-export const getFormStringOnChange = (form: UseFormReturnType<any>, path: string) => (form.getInputProps(path).onChange as (newValue: string) => void);
+export const getFormError = <T, Key extends keyof T>(form: UseFormReturnType<T>, path: Key) =>
+  (form.getInputProps(path).error as string | undefined);
 
-export const getFormDateValue = (form: UseFormReturnType<any>, path: string) => {
-  return form.getInputProps(path).value as Date;
-};
+export const getFormLocationValue = <T extends { location: unknown }>(form: UseFormReturnType<T>) =>
+  (form.getInputProps('location').value as CreateLocationType | null);
 
-export const getFormDateOnChange = (form: UseFormReturnType<any>, path: string) => (form.getInputProps(path).onChange as (newValue: Date) => void);
-
-export const getFormError = (form: UseFormReturnType<any>, path: string) => (form.getInputProps(path).error as string | undefined);
-
-export const getFormLocationValue = (form: UseFormReturnType<any>) => (form.getInputProps('location').value as CreateLocationType | null);
-
-export const getFormLocationOnChange = (form: UseFormReturnType<any>) => (form.getInputProps('location').onChange as (newValue: CreateLocationType | null) => void);
+export const getFormLocationOnChange = <T extends { location: unknown }>(form: UseFormReturnType<T>) =>
+  (form.getInputProps('location').onChange as (newValue: CreateLocationType | null) => void);
