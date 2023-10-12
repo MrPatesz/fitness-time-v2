33c5cf0c3d7436ab3ea49b4e65004dccd94b9028
Patrik Ángyán <patesz1999@gmail.com>
2023-10-13 00:10:08 +0200

    RichTextField fixed

--- src/components/rich-text/RichTextField.tsx
+++ src/components/rich-text/RichTextField.tsx
@@ -1,10 +1,10 @@
-import {ActionIcon, Group, TextInput} from '@mantine/core';
+import {ActionIcon, Group, Modal, TextInput, useMantineTheme} from '@mantine/core';
+import {UseFormReturnType} from '@mantine/form';
+import {useMediaQuery} from '@mantine/hooks';
 import {useTranslation} from 'next-i18next';
-import {FunctionComponent} from 'react';
+import {FunctionComponent, useState} from 'react';
 import {PencilPlus, X} from 'tabler-icons-react';
 import RichTextEditor from './RichTextEditor';
-import {UseFormReturnType} from '@mantine/form';
-import {openModal} from '@mantine/modals';
 
 export const RichTextField: FunctionComponent<{
   label?: string;
@@ -12,49 +12,59 @@ export const RichTextField: FunctionComponent<{
   maxLength?: number;
   formInputProps: ReturnType<UseFormReturnType<unknown>['getInputProps']>;
 }> = ({label, placeholder, formInputProps, maxLength = 1024}) => {
+  const theme = useMantineTheme();
+  const xs = useMediaQuery(`(min-width: ${theme.breakpoints.xs})`);
   const {t} = useTranslation('common');
+
+  const [open, setOpen] = useState(false);
 
   const value = formInputProps.value as string | undefined;
   const onChange = formInputProps.onChange as (newValue: string) => void;
 
   return (
-    <TextInput
-      label={label}
-      placeholder={placeholder}
-      sx={{flexGrow: 1}}
-      {...formInputProps}
-      readOnly={value?.includes('</')}
-      maxLength={maxLength}
-      rightSectionWidth={74}
-      rightSection={(
-        <Group spacing={4}>
-          <ActionIcon
-            title={t('button.reset')}
-            disabled={!value}
-            variant="transparent"
-            onClick={() => onChange('')}
-          >
-            <X/>
-          </ActionIcon>
-          <ActionIcon
-            title={t('button.richText')}
-            variant="transparent"
-            onClick={() => openModal({
-              title: label ? t('modal.edit', {propertyName: label}) : t('modal.comment.edit'),
-              children: (
-                <RichTextEditor
-                  placeholder={placeholder}
-                  value={value}
-                  onChange={onChange}
-                />
-              ),
-              size: 'xl',
-            })}
-          >
-            <PencilPlus/>
-          </ActionIcon>
-        </Group>
-      )}
-    />
+    <>
+      <TextInput
+        label={label}
+        placeholder={placeholder}
+        sx={{flexGrow: 1}}
+        {...formInputProps}
+        readOnly={value?.includes('</')}
+        maxLength={maxLength}
+        rightSectionWidth={74}
+        rightSection={(
+          <Group spacing={4}>
+            <ActionIcon
+              title={t('button.reset')}
+              disabled={!value}
+              variant="transparent"
+              onClick={() => onChange('')}
+            >
+              <X/>
+            </ActionIcon>
+            <ActionIcon
+              title={t('button.richText')}
+              variant="transparent"
+              onClick={() => setOpen(true)}
+            >
+              <PencilPlus/>
+            </ActionIcon>
+          </Group>
+        )}
+      />
+      <Modal
+        size="xl"
+        zIndex={401}
+        opened={open}
+        fullScreen={!xs}
+        onClose={() => setOpen(false)}
+        title={label ? t('modal.edit', {propertyName: label}) : t('modal.comment.edit')}
+      >
+        <RichTextEditor
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+      </Modal>
+    </>
   );
 };
