05c043440e1fe9dd983297dac6d51bb5755b55f0
Patrik Ángyán <patesz1999@gmail.com>
2023-10-13 11:02:08 +0200

    RichText sanitization

--- src/models/Utils.ts
+++ src/models/Utils.ts
@@ -1,12 +1,20 @@
+import sanitizeHtml from 'sanitize-html';
 import {z} from 'zod';
 
 export const IdSchema = z.number().min(1);
 
 export const NameSchema = z.string().trim().min(1).max(64);
 
-export const TextSchema = z.string().trim().min(1).max(512);
+export const TextSchema = z.string()
+  .trim()
+  .min(1)
+  .max(512)
+  .transform(dirtyHtml => sanitizeHtml(dirtyHtml));
 
-export const DescriptionSchema = z.string().trim().max(1024);
+export const DescriptionSchema = z.string()
+  .trim()
+  .max(1024)
+  .transform(dirtyHtml => sanitizeHtml(dirtyHtml));
 
 export const ImageSchema = z.union([
   z.string().trim().max(256).url(),
