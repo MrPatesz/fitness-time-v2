8d1a114cead26070c07a5c1f4aad2a52f8a53a7d
Patrik Ángyán <patesz1999@gmail.com>
2023-11-23 23:08:06 +0100

    more EventForm tests

--- tests/client/components/event/EventForm.test.tsx
+++ tests/client/components/event/EventForm.test.tsx
@@ -1,10 +1,10 @@
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import {EventForm} from '../../../../src/components/event/EventForm';
 import {api} from '../../../../src/utils/api';
 import {getDefaultCreateEvent} from '../../../../src/utils/defaultObjects';
 
 describe('EventForm', () => {
-  it('has disabled submit button initially', () => {
+  beforeEach(() => {
     // Arrange
     const Component = api.withTRPC(() => (
       <EventForm
@@ -14,12 +14,32 @@ describe('EventForm', () => {
       />
     ));
     render(<Component/>);
+  });
 
+  it('has disabled submit button initially', () => {
     // Act
     const submitButton = screen.getByText('button.submit').closest('button');
 
     // Assert
     expect(submitButton).toBeDisabled();
   });
-  // TODO more tests
+
+  it('has disabled reset button initially', () => {
+    // Act
+    const resetButton = screen.getByText('button.reset').closest('button');
+
+    // Assert
+    expect(resetButton).toBeDisabled();
+  });
+  it('has enabled reset button after form becomes dirty', () => {
+    // Arrange
+    const descInput = screen.getByLabelText('eventForm.description.label');
+    fireEvent.change(descInput, {target: {value: 'test event'}});
+
+    // Act
+    const resetButton = screen.getByText('button.reset').closest('button');
+
+    // Assert
+    expect(resetButton).not.toBeDisabled();
+  });
 });
